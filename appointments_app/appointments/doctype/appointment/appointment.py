b37c9f19f8d3dc945d8f1e392a4647d604bfdb1c
Priya Thakur <priyathakur1823@gmail.com>
2024-11-06 18:15:06 +0530

    Initial commit for doctor app

---
+++ appointments_app/appointments/doctype/appointment/appointment.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2024, priya and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class Appointment(Document):
+	def after_insert(self):
+		print("Your queue number is : ", self.add_to_appointment_queue())
+
+	def add_to_appointment_queue(self):
+		q = frappe.get_doc("Appointment Queue", {
+			"date": self.date,
+			"shift": self.shift,
+			"clinic": self.clinic
+		})
+
+		q.append("queue", {"appointment": self.name, "status": "Pending"})
+		q.save(ignore_permissions=True)
+
+		return len(q.queue)
